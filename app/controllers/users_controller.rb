d919a85e28f5565b8cd8f28d6f53661f382f387d
pirvudoru <doru.pirvu@agilefreaks.com>
2015-06-06 18:01:45 +0300

    [#3] adds endpoint to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   respond_to :json
 
   def create
-    respond_with User.new
+    respond_with User.create!(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_number)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,6 +6,38 @@ describe UsersController do
     let(:params) { { format: format } }
     subject { post :create, params }
 
-    its(:status) { is_expected.to eq(201) }
+    context 'user parameters are defined' do
+      before do
+        params[:user] = {
+            name: 'test',
+            email: 'email@user.com',
+            phone_number: '012345'
+        }
+      end
+
+      its(:status) { is_expected.to eq(201) }
+
+      it 'saves a new user' do
+        expect { subject }.to change { User.count }.by(1)
+      end
+
+      it 'assigns name to user' do
+        subject
+
+        expect(User.last.name).to eq(params[:user][:name])
+      end
+
+      it 'assigns email to user' do
+        subject
+
+        expect(User.last.email).to eq(params[:user][:email])
+      end
+
+      it 'assigns phone_number to user' do
+        subject
+
+        expect(User.last.phone_number).to eq(params[:user][:phone_number])
+      end
+    end
   end
 end
