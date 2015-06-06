4409e356008839f1e19868124f8fdd908e2631b9
pirvudoru <doru.pirvu@agilefreaks.com>
2015-06-06 22:13:11 +0300

    [#4] adds geospatial location matching

--- lib/jobs/handshake_match_job.rb
+++ lib/jobs/handshake_match_job.rb
@@ -3,11 +3,13 @@ class HandshakeMatchJob
 
   def perform
     Handshake.all.each do |handshake|
-      matching_handshake = Handshake.where({
-                                               :timestamp.gte => handshake.timestamp - HANDSHAKE_WINDOW,
-                                               :timestamp.lte => handshake.timestamp + HANDSHAKE_WINDOW,
-                                               :_id.ne => handshake._id
-                                           })
+      matching_handshake = Handshake
+                               .where({
+                                          :timestamp.gte => handshake.timestamp - HANDSHAKE_WINDOW,
+                                          :timestamp.lte => handshake.timestamp + HANDSHAKE_WINDOW,
+                                          :_id.ne => handshake._id
+                                      })
+                               .geo_near(handshake.location).max_distance(0.00005) # 1 m = 0.0000005
                                .first
 
       if matching_handshake
--- spec/lib/jobs/handshake_match_job_spec.rb
+++ spec/lib/jobs/handshake_match_job_spec.rb
@@ -9,9 +9,10 @@ describe HandshakeMatchJob do
       let!(:user1) { User.create!(name: 'First') }
       let!(:user2) { User.create!(name: 'Second') }
 
-      let!(:handshake1) { user1.handshakes.create!(timestamp: Time.now + 11.seconds) }
-      let!(:handshake2) { user2.handshakes.create!(timestamp: Time.now + 30.seconds) }
-      let!(:handshake3) { user2.handshakes.create!(timestamp: Time.now + 5.seconds) }
+      let!(:handshake1) { user1.handshakes.create!(timestamp: Time.now + 11.seconds, location: [45.784496, 24.171597]) }
+      let!(:handshake2) { user2.handshakes.create!(timestamp: Time.now + 30.seconds, location: [45.784499, 24.171607]) }
+      let!(:handshake3) { user2.handshakes.create!(timestamp: Time.now + 5.seconds, location: [45.784498, 24.171603]) }
+      let!(:handshake4) { user2.handshakes.create!(timestamp: Time.now + 5.seconds, location: [47.784499, 23.171607]) }
 
       it 'adds user2 acquaintance to user1' do
         subject
@@ -31,7 +32,7 @@ describe HandshakeMatchJob do
         subject
 
         expect(user1.reload.handshakes.count).to eq(0)
-        expect(user2.reload.handshakes).to match_array([handshake2])
+        expect(user2.reload.handshakes).to match_array([handshake2, handshake4])
       end
     end
   end
