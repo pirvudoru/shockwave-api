60d174dd1f01db3f98c369d77ec47e68c206c7e1
pirvudoru <doru.pirvu@agilefreaks.com>
2015-06-06 21:23:57 +0300

    [#4] deletes matched handshakes

--- lib/jobs/handshake_match_job.rb
+++ lib/jobs/handshake_match_job.rb
@@ -3,14 +3,19 @@ class HandshakeMatchJob
 
   def perform
     Handshake.all.each do |handshake|
-      matching_handshake = Handshake.find_by({
-                                              :timestamp.gte => handshake.timestamp - HANDSHAKE_WINDOW,
-                                              :timestamp.lte => handshake.timestamp + HANDSHAKE_WINDOW
-                                          })
+      matching_handshake = Handshake.where({
+                                               :timestamp.gte => handshake.timestamp - HANDSHAKE_WINDOW,
+                                               :timestamp.lte => handshake.timestamp + HANDSHAKE_WINDOW,
+                                               :_id.ne => handshake._id
+                                           })
+                               .first
 
       if matching_handshake
         handshake.user.acquaintances.push(matching_handshake.user)
         matching_handshake.user.acquaintances.push(handshake.user)
+
+        handshake.destroy
+        matching_handshake.destroy
       end
     end
   end
--- spec/lib/jobs/handshake_match_job_spec.rb
+++ spec/lib/jobs/handshake_match_job_spec.rb
@@ -9,22 +9,29 @@ describe HandshakeMatchJob do
       let!(:user1) { User.create!(name: 'First') }
       let!(:user2) { User.create!(name: 'Second') }
 
-      before do
-        user1.handshakes.create!(timestamp: Time.now + 11.seconds)
-        user2.handshakes.create!(timestamp: Time.now + 12.seconds)
-        user2.handshakes.create!(timestamp: Time.now + 11.seconds)
-      end
+      let!(:handshake1) { user1.handshakes.create!(timestamp: Time.now + 11.seconds) }
+      let!(:handshake2) { user2.handshakes.create!(timestamp: Time.now + 30.seconds) }
+      let!(:handshake3) { user2.handshakes.create!(timestamp: Time.now + 5.seconds) }
 
       it 'adds user2 acquaintance to user1' do
         subject
 
         expect(user1.reload.acquaintances).to include(user2)
+        expect(user1.reload.acquaintances.count).to eq(1)
       end
 
       it 'adds user1 acquaintance to user2' do
         subject
 
         expect(user2.reload.acquaintances).to include(user1)
+        expect(user2.reload.acquaintances.count).to eq(1)
+      end
+
+      it 'deletes matched handshakes' do
+        subject
+
+        expect(user1.reload.handshakes.count).to eq(0)
+        expect(user2.reload.handshakes).to match_array([handshake2])
       end
     end
   end
